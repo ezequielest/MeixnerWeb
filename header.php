1c6f45453761a33a42565dd5c9cb57d15057e82f
ezequielest <ezequiel.estigarribia@gmail.com>
2018-06-04 19:39:37 -0300

    Se estructura el proyecto a php

---
+++ header.php
@@ -0,0 +1,89 @@
+<!--Update: 29-12-15-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+	<meta name="keywords" content="construccion de viviendas, departamentos, arquitectura, casas, edificios">
+	<meta name="description" content="Nos dedicamos a la proyección, dirección y construcción de obras, embarcándonos en desarrollos inmobiliarios">
+
+	<title>Alwind</title>
+
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/fakeLoader.css">
+	<link rel="stylesheet" href="css/meixner.css">
+	<link rel="stylesheet" href="css/style-icon.css">
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="css/gridstack.css">
+		<link rel="stylesheet" href="css/gridstack-extra.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="apple-touch-icon" sizes="57x57" href="img/icon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="img/icon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/icon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="img/icon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/icon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="img/icon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="img/icon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="img/icon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="img/icon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="img/icon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="img/icon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="img/icon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="img/icon/favicon-16x16.png">
+	<link rel="manifest" href="img/icon/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="img/icon/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+</head>
+
+<body>
+
+<!--<div id="fakeLoader"></div>-->
+
+<nav id="header" class="navbar navbar-fixed-top estilo" data-sr="wait 1.0s">
+	<div id="header-container" class="container sin-padding navbar-container">
+						<div class="navbar-header">
+								<!--<a id="brand" class="navbar-brand" href="#">
+									<img src="img/logos/meixner.png"/>
+								</a>-->
+							<div id="contenedor-logo" class="container logo">
+
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+										<span class="sr-only">Toggle navigation</span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+								</button>
+								<a id="brand" class="navbar-brand hidden-xs grande" href="#">
+								     <img class="logoprincipal center-block" src="img/logos/meixnergroup-n-550x50.png"/>
+							    </a>
+				 				 <!--brand phones-->
+							    <a id="brand" class="navbar-brand visible-xs-block" href="#">
+								     <img src="img/logos/meixnergroup-n-220x20.png"/>
+							    </a>
+							</div>     
+						</div>
+	</div>
+	<div id="navbar" class="collapse navbar-collapse">
+
+					<!--<ul id="redes" class="nav navbar-nav navbar-right">
+						<li><a href="#"><span class="icon-facebook"></span><span class="sr-only">(current)</span></a></li>
+						<li><a href="#"><span class="icon-twitter"></span></a></li>
+						<li><a href="#"><span class="icon-linkedin2"></span></a></li>
+					</ul>-->
+ 					<div class="container-menu">
+						<ul id="menu" class="nav navbar-nav">
+							<li><a href="index.html">Home<span class="sr-only">(current)</span></a></li>
+							<li><a href="index.html#meix">Quienes somos</a></li>
+							<li><a href="portfolio.html">Proyectos</a></li>
+							<li class="active"><a href="index.html#servicios">Servicios</a></li>
+							<li><a href="index.html#clientes">Equipo</a></li>
+							<li><a href="index.html#prensa">Prensa</a></li>
+							<li><a href="#contacto">Contacto</a></li>
+						</ul>
+					</div>
+	</div>				
+</nav><!-- /.navbar -->
---
+++ hormeix.php
@@ -0,0 +1,123 @@
+<?php include('header.php'); ?>
+<?php include('header.php'); ?>
+
+<section id="espacionav"></section>
+
+<section id="hormeix" class="container-fluid contenedor-servicios" data-sr="wait 0.2s">
+<div class="container">
+		<div class="row">
+				  <div class="col-md-12">
+						  <div class="contenedor">
+						      <img class="logo img-responsive" src="img/logos/hormeix.png"/>
+							</div>
+		      </div>
+		</div>
+		<div class="row separador">
+					<div class="col-md-12">
+									<hr class="animated zoomIn">
+					</div>
+		</div>
+		<div class="row">
+					<div class="col-sm-7 col-md-8 sin-right">
+            <div id="owl-hormeix" class="owl-carousel owl-theme">
+              <div class="item item1"></div>
+              <div class="item item2"></div>
+              <div class="item item3"></div>
+              <div class="item item4"></div>
+              <div class="item item5"></div>
+              <div class="item item6"></div>
+            </div>
+
+            <!-- <div class="customNavigation">
+              <a class="btn prev">Previous</a>
+              <a class="btn next">Next</a>
+              <a class="btn play">Autoplay</a>
+              <a class="btn stop">Stop</a>
+            </div> -->
+					</div>
+          	<div class="col-sm-5 col-md-4 sin-left">
+            	<div class="info">
+              		<h3 class="hormeix">Hormeix</h3>
+					<p data-sr="wait 0.2s">Este es el caso de nuestra División dedicada a la elaboración y distribución de los distintos tipos de hormigón elaborado. Equipados con maquinaria especializada, regidos por las normas de calidad y métodos de trabajo desarrollados a través de nuestra experiencia, aseguramos un servicio de alta calidad y perdurabilidad.</p>
+            	</div>
+			</div>
+		</div>
+</div>
+</section>
+
+
+<footer id="contacto" class="container-fluid">
+	<div class="container">
+		<div class="row" data-sr="wait 0.5s">
+			<div class="col-sm-6 col-md-6">
+				<div class="fila-datos">
+					<div class="row">
+						<div class="col-sm-1 col-md-1 col-lg-1 location">
+							<span class="fa fa-map-marker hidden-xs" aria-hidden="true"></span>
+						</div>
+						<div class="col-sm-11 col-md-11 col-lg-11">
+							<div class="datos">
+								<h3>Datos de contacto</h3>
+								<ul class="datos">
+									<li>Alberdi 75, 1878 Quilmes, Buenos Aires, Argentina</li>
+									<li><a href="mailto:contacto@meixnergroup.com">contacto@meixnergroup.com</a></li>
+									<li>+54 (011) 4224-0785</li>
+								</ul>
+								<ul class="redes">
+									<li><a href="https://www.facebook.com/mt2arquitectura/"><span class="fa fa-facebook" aria-hidden="true"></span></a></li>
+									<li><span class="fa fa-linkedin" aria-hidden="true"></span></li>
+									<li><a href=""><span class="fa fa-google-plus" aria-hidden="true"></span></a></li>
+								</ul>
+							</div>
+						</div>
+								<!-- <span class="icon-mobile2"></span>011 156344935 -->
+             		</div>
+             		<div class="row">
+             			<div class="col-md-12">
+             				<img class="qr hidden-xs" src='img/qr-meix.png' alt='QR Code'/>
+             			</div>
+             		</div>
+				</div>
+			</div>
+			<div class="col-sm-6 col-md-6">
+				<div class="formulario">
+					<h3>Formulario</h3>
+					<input type="text" id="nombre" name="nombre" value="" placeholder="Nombre y Apellido">
+					<input type="text" id="telefono" name="telefono" value="" placeholder="Telefono">
+					<input type="email" id="email" name="email" value="" placeholder="E-mail">
+					<textarea id="mensaje" placeholder="Mensaje"></textarea>
+					<div class="row">
+					  <div class="col-md-3"><input id="btn-enviar" type="button" name="name" value="Enviar"></div>		
+					  <div class="col-md-9"><span id="respuesta"></span></div>		
+	 				
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+
+<section id="copy" class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			 	<h5>© 2016 | Meixner Group</h5>
+		</div>
+	</div>
+</section>
+
+<script src="js/jquery-2.1.4.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+<script src="js/fakeLoader.js"></script>
+<script src="js/scrollReveal.js"></script>
+<script src="js/lodash.js"></script>
+<script src="js/owl.carousel.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/scroll.js"></script>
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/meixner.js"></script>
+<script src="js/gridstack.js"></script>
+<script src="js/cambiologo.js"></script>
+
+</body>
+
+</html>
